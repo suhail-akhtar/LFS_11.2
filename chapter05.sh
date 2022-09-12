eef3c8dba3f24925bfc9482177c1eb63e328ae26
Suhail Akhtar <suhailakhtar@live.com>
2022-09-12 15:53:30 +0500

    Add files via upload

---
+++ chapter05.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+$LFS/sources/LFS/chapter05/01-binutils-2.39.sh
+
+$LFS/sources/LFS/chapter05/02-gcc-12.2.0.sh
+
+$LFS/sources/LFS/chapter05/03-linux-5.19.2.sh
+
+$LFS/sources/LFS/chapter05/04-glibc-2.36.sh
+
+$LFS/sources/LFS/chapter05/05-gcc-12.2.0.sh
