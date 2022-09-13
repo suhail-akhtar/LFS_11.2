b875af1e69e936cb460250cfd27e64603ca662f8
Suhail Akhtar <suhailakhtar@live.com>
2022-09-13 15:38:26 +0500

    LFS Enter to chroot

---
+++ enter_chroot.sh
@@ -0,0 +1,23 @@
+export LFS=/mnt/lfs
+
+umount $LFS/dev/pts
+umount $LFS/{sys,proc,run,dev}
+
+mkdir -pv $LFS/{dev,proc,sys,run}
+mount -v --bind /dev $LFS/dev
+mount -v --bind /dev/pts $LFS/dev/pts
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
+
+if [ -h $LFS/dev/shm ]; then
+  mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+fi
+
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    /bin/bash --login
+
